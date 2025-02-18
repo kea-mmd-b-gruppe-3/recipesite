a67e2a0a70061896b33c97ebdfc80c51ccdedd29
victoriawerngreen <viggomussen@gmail.com>
2025-02-18 14:50:46 +0100

    add filter knapper

--- js/idcards.js
+++ js/idcards.js
@@ -1,15 +1,27 @@
-// ------------ filterings funktion altså option
-// const selectElement = document.querySelector("selectElement");
-// selectElement.addEventListener("change");
-
+const myMealType = new URLSearchParams(window.location.search).get("meal-type");
 
 // ------------ normal json
 const listContainer = document.querySelector(".grid_1-1-1");
 
-fetch(`https://dummyjson.com/recipes`)
+fetch(`https://dummyjson.com/recipes/meal-type/${myMealType}`)
   .then((response) => response.json())
   // skriver () efter .json fordi det er en funktion der allerede findes og vi kalder på den via ()
-  .then((product) => showList(product.recipes));
+  .then((json) => {
+    allData = json.recipes;
+    showList(allData);
+  });
+
+document.querySelectorAll("button").forEach((knap) => knap.addEventListener("click", showFiltered));
+
+function showFiltered() {
+  const filter = this.dataset.difficulty;
+  if (filter == "All") {
+    showList(allData);
+  } else {
+    fraction = allData.filter((product) => product.difficulty === filter);
+    showList(fraction);
+  }
+}
 
 function showList(products) {
   console.log(products);
