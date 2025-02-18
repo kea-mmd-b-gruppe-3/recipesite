f0dc89f472990457987d18164bda9ab7976bbe01
Helena Troland <hetr0001@stud.kea.dk>
2025-02-18 13:58:00 +0100

    add changes med js

--- js/index.js
+++ js/index.js
@@ -1,30 +1,30 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let mainContainer = document.querySelector("main");
+// document.addEventListener("DOMContentLoaded", function () {
+//   let mainContainer = document.querySelector("main");
 
-  const markup = `
-      <div class="whisk">
-        <img src="/img/whisk.svg" alt="whisk" />
-      </div>
-  
-      <h1>WELCOME TO MAMA’S KITCHEN</h1>
-  
-      <div class="text">
-        <p>I’m so happy you’re here.</p>
-        <p>I’m a stay-at-home mom with a deep love for home cooking. After years of making meals for my family, experimenting in the kitchen, and collecting recipes, I finally decided to share them with the world. And that’s how MAMA’S KITCHEN was born!</p>
-        <p>This site is all about simple, delicious, and family-friendly recipes that bring joy to the table. Whether you’re looking for quick weeknight dinners, comforting baked goods, or fun treats for the kids, you’ll find something here to make mealtime special.</p>
-        <p>I believe that cooking doesn’t have to be complicated to be amazing. With easy-to-follow recipes, helpful tips, and a sprinkle of love.</p>
-        <p>I hope to inspire you to get into the kitchen and create meals that make you and your family smile.</p>
-        <p>So, grab an apron, explore the recipes, and let’s make every bite a happy one.</p>
-        <p>Welcome to MAMA’S KITCHEN!</p>
-      </div>
-  
-      <div class="plate">
-        <img src="/img/plate.svg" alt="plate" />
-        <a href="/html/categories.html">
-          <p>DISCOVER HERE!</p>
-        </a>
-      </div>
-    `;
+//   const markup = `
+//       <div class="whisk">
+//         <img src="/img/whisk.svg" alt="whisk" />
+//       </div>
 
-  mainContainer.innerHTML = markup;
-});
+//       <h1>WELCOME TO MAMA’S KITCHEN</h1>
+
+//       <div class="text">
+//         <p>I’m so happy you’re here.</p>
+//         <p>I’m a stay-at-home mom with a deep love for home cooking. After years of making meals for my family, experimenting in the kitchen, and collecting recipes, I finally decided to share them with the world. And that’s how MAMA’S KITCHEN was born!</p>
+//         <p>This site is all about simple, delicious, and family-friendly recipes that bring joy to the table. Whether you’re looking for quick weeknight dinners, comforting baked goods, or fun treats for the kids, you’ll find something here to make mealtime special.</p>
+//         <p>I believe that cooking doesn’t have to be complicated to be amazing. With easy-to-follow recipes, helpful tips, and a sprinkle of love.</p>
+//         <p>I hope to inspire you to get into the kitchen and create meals that make you and your family smile.</p>
+//         <p>So, grab an apron, explore the recipes, and let’s make every bite a happy one.</p>
+//         <p>Welcome to MAMA’S KITCHEN!</p>
+//       </div>
+
+//       <div class="plate">
+//         <img src="/img/plate.svg" alt="plate" />
+//         <a href="/html/categories.html">
+//           <p>DISCOVER HERE!</p>
+//         </a>
+//       </div>
+//     `;
+
+//   mainContainer.innerHTML = markup;
+// });
